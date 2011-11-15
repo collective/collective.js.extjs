ad451e76258f200ad90db293f17a3fce12ecc039
Alex Clark <aclark@aclark.net>
2011-11-15 16:27:43 -0500

    First cut

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='collective.js.extjs',
+    namespace_packages=[
+        'collective',
+        'collective.js'
+    ],
+    version=0.1.0,
+    packages=find_packages(),
+)
